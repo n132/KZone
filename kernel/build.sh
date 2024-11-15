8d0de035870c7e1f51e4bdab48fa1f846b61213b
n132 <xm2146@nyu.edu>
2024-11-14 19:45:53 -0700

    chore: build: Enable debug by default

--- kernel/build.sh
+++ kernel/build.sh
@@ -1,4 +1,4 @@
 echo "Downloading the source code of" $1
 rm -rf kernel-$1
 git clone --branch $1 --depth 1 git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git kernel-$1
-docker run -v`realpath ./kernel-$1`:/mnt  n132/kernel-compiler:20.04 bash -c "cd /mnt && make defconfig && make -j128"
+docker run -v`realpath ./kernel-$1`:/mnt  n132/kernel-compiler:20.04 bash -c "cd /mnt && make defconfig && echo 'CONFIG_DEBUG_INFO=y' >> ./.config && make -j128"
